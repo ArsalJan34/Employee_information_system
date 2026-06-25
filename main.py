c5ece3caadd403d1cc80184d3117c5e7cf7a9e1f
ArsalJan34 <arsaljaan532@gmail.com>
2026-06-25 15:59:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+first_name = input("Enter First Name : ")
+last_name = input("Enter Last Name :")
+Address = input("Enter address : ")
+apartment = input("Enter apratment number : ")
+age = int(input("Enter Age : "))
+experience = int(input("Enter Experience : "))
+position = input("Enter The Position : ")
+salary = input("Enter The Salary : ")
+
+
+Employee = last_name + " " + last_name
+print("Employee : ", Employee)
+Employee_age = f"{age}"
+print("Age : ", Employee_age)
+Employee_position = position.title()
+print("Position : ",Employee_position)
+Employee_salary = f"${salary}"
+print("Salary : ", Employee_salary)
+Employee_experience = f"{experience} Years "
+print()
